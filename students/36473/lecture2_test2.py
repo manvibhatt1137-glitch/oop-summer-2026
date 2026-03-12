a1c61e89e6bd4c29c65f18d4a58a35c42b9a88f2
manvi bhatt 36473 <manvibhatt1137@gmail.com>
2026-03-12 10:20:49 +0000

    test2

---
+++ students/36473/lecture2_test2.py
@@ -0,0 +1,13 @@
+my_birthday=11
+print(my_birthday)
+print(type(my_birthday))
+
+my_name='manvi bhatt'
+print(my_name)
+print(type(my_name))
+
+my_grade= 4.5
+print(type(my_grade))
+new_grade=int(my_grade)
+print(new_grade)
+print(type(new_grade))
