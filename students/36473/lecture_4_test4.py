988f8c4a1f25a95ae590935001da76289c2e2a6b
manvi bhatt 36473 <manvibhatt1137@gmail.com>
2026-03-26 11:21:51 +0100

    Create lecture_4_test4.py

---
+++ students/36473/lecture_4_test4.py
@@ -0,0 +1,14 @@
+class MyFruit:
+    my_first_fruit = "apple"
+    my_second_fruit  = "banana"
+    my_third_fruit = "cranberry"
+
+first = MyFruit()
+second = MyFruit()
+third = MyFruit()
+
+print(first.my_first_fruit)
+print(second.my_second_fruit)
+print(third.my_third_fruit)
+
+#these are the fruits
