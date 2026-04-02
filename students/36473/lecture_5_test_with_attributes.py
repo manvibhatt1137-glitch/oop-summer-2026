f2e22a0b332ed9779709ec80e5a9b70fd8d067fa
manvi bhatt 36473 <manvibhatt1137@gmail.com>
2026-04-02 10:19:06 +0200

    Create lecture_5_test_with_attributes.py

---
+++ students/36473/lecture_5_test_with_attributes.py
@@ -0,0 +1,8 @@
+#class with object attribute
+class MySelf:
+    def __init__(self,name,age):
+        self.name= name
+        self.age = age
+p1 = MySelf("manvi",18)
+print(p1.name)
+print(p1.age)
