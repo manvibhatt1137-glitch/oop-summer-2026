c153d5646115101bdbd9b8537e2d51562339caf5
manvi bhatt 36473 <manvibhatt1137@gmail.com>
2026-04-09 10:17:31 +0200

    Create test_06_empty_class.py

---
+++ students/36473/test_06_empty_class.py
@@ -0,0 +1,9 @@
+class CardDetails:
+    pass
+p1 = CardDetails()
+p1.name = "kara"
+p1.card_type = "debit card"
+p1.card_number = 45572384677
+print(p1.name)
+print(p1.card_number)
+print(p1.card_type)
