46620aa151e48a2aa6fae104ea4896f564615711
Selim Yildiz <97531756+667revo@users.noreply.github.com>
2026-04-09 10:14:12 +0200

    Add Lecture class and instance attributes
    empty class

---
+++ students/34551/test_06_empty_class.py
@@ -0,0 +1,10 @@
+class Lecture:
+    pass
+
+l1 = Lecture()
+l1.lecture = "OOP"
+l1.name = "Mustafa Selim Yildiz"
+l1.id = "34551"
+l1.attendance = True
+
+print(l1.lecture, l1.name, l1.id, l1.attendance)
